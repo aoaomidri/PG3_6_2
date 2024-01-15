6dd60642ea4eb7687e9d654e159ce1403d3e3e4d
aoaomidri <aoaomidori2723@gmail.com>
2024-01-15 10:23:57 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<thread>
+void DrawThread(int num) {
+    std::cout << "thread " << num << std::endl;
+}
+
+void DrawThreadAddOne(int num) {
+    num += 1;
+    DrawThread(num);
+}
+
+void DrawThreadAddTwo(int num) {
+    num += 2;
+    DrawThread(num);
+}
+
+int main(){
+    int num = 1;
+    //マルチスレッド
+    std::thread th1(DrawThread, num);
+    th1.join();
+    std::thread th2(DrawThreadAddOne, num);
+    th2.join();
+    std::thread th3(DrawThreadAddTwo, num);
+    th3.join();
+
+    return 0;
+}
